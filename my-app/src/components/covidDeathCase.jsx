95ad6020017a88ad02826ad70dead04e362d0bea
adarshalimbu <adarshalimbu@gmail.com>
2021-11-17 21:29:23 +0000

    npm ant d install

---
+++ my-app/src/components/covidDeathCase.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Card } from 'antd';
+
+export const covidDeathCase = () => {
+    return (
+        <div>
+            <Card title="Total Death Cases"></Card>
+        </div>
+    )
+}
+
+export default covidDeathCase;
