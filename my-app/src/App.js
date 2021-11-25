04a2a2b435cf24a7dc4e314d8d484303dfabb752
Manjil Ghale <rilleymanjil@hotmail.com>
2021-11-25 18:15:29 +0000

    Connected with REST API
    Used axios to fetch API data from our rest api into our react client file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,46 @@
 import { Cards, Chart, CountryPicker } from './components';
 import Css from './App.module.css';
 import React from 'react';
+import axios from 'axios'
 
 import logo from './images/covid-19.svg'
 
 class App extends React.Component {
+
+  state = {
+    title: '',
+    body: '',
+    posts: []
+  }
+  
+  componentDidMount = () => {
+    this.getCovidData();
+  }
+
+  getCovidData = () => {
+    axios.get('http://localhost:8080/all-covid-Data')
+      .then((response) => {
+        const data = response.data;
+        this.setState({ posts: data });
+        console.log("Data has been recieved");
+        console.log(data);
+      }) 
+      .catch(() => {
+        console.log("Error retrieving data");
+      })
+  }
+
+  displayData = (posts) => {
+
+    if (!posts.length) return null;
+
+    return posts.map((post, index) => (
+      <div key={index}>
+        <h3>Je;;p</h3>
+      </div>
+    ));
+  }
+
   render() {
     return (
       <div className={Css.container}>
--- my-app/src/services/index.js
+++
@@ -1,9 +0,0 @@
-
-fetch('http://localhost:8080/all-covid-Data')
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => {
-        console.log(err);
-    })
-
-exports.module = fetch;
